d09842e17dd37b50c306a066e353ed824374f39b
abbasghori29 <132843772+abbasghori29@users.noreply.github.com>
2024-07-23 22:36:55 +0500

    Initial commit

--- mainProject/courseCompassApp/views.py
+++ mainProject/courseCompassApp/views.py
@@ -5,8 +5,12 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
+from langchain.memory import ConversationBufferMemory
+from django.shortcuts import get_object_or_404
+from langchain_core.output_parsers import StrOutputParser
+from .models import *
 import os
+
 
 os.environ['LANGCHAIN_API_KEY'] = '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'
 os.environ["Google_API_KEY"]='AIzaSyASa7wfJJ9elN-R837UEwUznB4wUnYm-b4'
@@ -25,42 +29,64 @@ new_db = FAISS.load_local("C:/Users/RTC/Desktop/clone/CourseCompass-AI-Chatbot/m
 
 def getSimilar_documents(query):
     retriever=new_db.as_retriever()
-    relevant_docs=retriever.get_relevant_documents(query)
+    relevant_docs=retriever.invoke(query)
     return relevant_docs
+def getPreviousConversation(email):
+    converations=User.objects.get(email=email).messages
+    return converations
 def generate_response(request):
-    
+    email="abbas@example.com"
+    conv=getPreviousConversation(email)
+    print(conv)
     if request.method == 'POST':
         data=json.loads(request.body)
         print(data)
         query=data['query']
-    
-        retriever=new_db.as_retriever()
-        prompt_template = """Given the following context and a question, generate an answer based on this context only.
-        In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
-        If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+        context=getSimilar_documents(query)
+        prompt_template = """
+        You are a helpful assistant who can understand and use context and previous chat history to answer questions.
 
-        CONTEXT: {context}
+        Here is the context from the source document:
+        {context}
 
-        QUESTION: {question}"""
+        Here is the history of the conversation so far:
+        {chat_history}
+
+        The user has asked the following question:
+        QUESTION: {question}
+
+        Instructions:
+            - Analyze the context and chat history to provide a well-thought-out and detailed response to the user's question.
+            - Do not include labels from chat history like "AI:" in the response.They are just for your understanding.
+            -
+            - Do not simply copy from the chat history; instead, synthesize information from both the context and chat history to construct a meaningful answer.
+            - If the answer is not directly found in the context or chat history, formulate a relevant response based on the information available, or clearly state "I don't know" if no reasonable answer can be provided.
+            - For greetings or common conversational starters, respond appropriately with "Hello! How can I help you?"
+
+            Your response should demonstrate understanding and provide additional insights where possible, rather than merely repeating previous exchanges.
+        """
+
 
 
         PROMPT = PromptTemplate(
-            template=prompt_template, input_variables=["context", "question"]
+            template=prompt_template, input_variables=["context", "question", "chat_history"]
         )
-        chain = RetrievalQA.from_chain_type(llm=llm ,
-                                        chain_type="stuff",
-                                        retriever=retriever,
-                                        input_key="query",
-                                        return_source_documents=True,
-                                        chain_type_kwargs={"prompt": PROMPT})
-        response=chain.invoke(query)
-        return JsonResponse({"response": response['result']},status=200)
+        
+        output_parser = StrOutputParser()
+
+        chain = PROMPT | llm | output_parser
+
+        response = chain.invoke({"context": context, "question": query,"chat_history": conv})
+        user = get_object_or_404(User, email=email)
+
+        user.messages.append({"Human: ": query,"AI: ": response})
+        user.save()
+        
+        return JsonResponse({"response": response},status=200)
     
     return JsonResponse({'message':'error'},status=500)
     
     
-
-
 def home(request):
     return render(request,'index.html')
     
